7890a582a384a1043d706c842598e0bbefdc2870
BrandenOlson <branden.olson@gmail.com>
2025-07-12 21:35:55 -0600

    tool call evals working :rocket:

--- evals/run_evals.py
+++ evals/run_evals.py
@@ -31,7 +31,14 @@ def evaluate_text(actual: str, expected: str | list) -> list[float]:
     return [100*int(expected in actual_text) for expected in expected_texts]
 
 
+
+def evaluate_tool_calls(actual: list[str], expected: list[str]) -> float:
+    # TODO: generalize to beyond exact match
+
+    return 100*int(actual == expected)
+
 expected_text_hits = []
+expected_tool_call_hits = []
 
 examples_with_metrics = deepcopy(EXAMPLES)
 
@@ -45,11 +52,20 @@ for example in examples_with_metrics:
             example_expected_text_scores = evaluate_text(normalize_text(response_text), turn["expected_text"])
             expected_text_hits.extend(example_expected_text_scores)
 
-        turn["includes_expected_text"] = example_expected_text_scores
+            turn["includes_expected_text"] = example_expected_text_scores
+
+        response_tool_calls = response.json()["tool_calls"]
+        turn["agent_tool_calls"] = response_tool_calls
+        if "expected_tool_calls" in turn:
+            example_expected_tool_call_score = evaluate_tool_calls(response_tool_calls, turn["expected_tool_calls"])
+            expected_tool_call_hits.append(example_expected_tool_call_score)
+
+            turn["includes_expected_tool_calls"] = example_expected_tool_call_score
 
 RESULTS_FILENAME = "eval_results.json"
 with open(RESULTS_FILENAME, "w") as f:
     json.dump(examples_with_metrics, f, indent=4)
 
-print(round(np.mean(expected_text_hits), 2))
+print(f"Text coverage: {round(np.mean(expected_text_hits), 2)}")
+print(f"Tool call coverage: {round(np.mean(expected_tool_call_hits), 2)}")
 
